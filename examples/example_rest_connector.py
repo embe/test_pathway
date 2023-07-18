86d63f1228aeebce1604630bda4508a1175cc8d5
Manul from Pathway <github.manul@pathway.com>
2023-07-18 11:22:59 +0200

    Project import generated by Copybara.
    GitOrigin-RevId: 48b08b6094c6a827a2d38a4cceb732df4052cf8c

---
+++ examples/example_rest_connector.py
@@ -0,0 +1,26 @@
+import os
+
+import pathway as pw
+
+HTTP_HOST = os.environ.get("PATHWAY_REST_CONNECTOR_HOST", "127.0.0.1")
+HTTP_PORT = os.environ.get("PATHWAY_REST_CONNECTOR_PORT", "8080")
+
+
+def logic(queries: pw.Table) -> pw.Table:
+    return queries.select(
+        query_id=queries.id, result=pw.apply(lambda x: x.upper(), pw.this.query)
+    )
+
+
+class InputSchema(pw.Schema):
+    query: str
+    user: str
+
+
+queries, response_writer = pw.io.http.rest_connector(
+    host=HTTP_HOST, port=int(HTTP_PORT), schema=InputSchema
+)
+responses = logic(queries)
+response_writer(responses)
+
+pw.run_all(debug=True, monitoring_level=pw.MonitoringLevel.NONE)
---
+++ python/pathway/internals/__init__.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2022 NavAlgo
+#
+# Proprietary and confidential.
+
+from __future__ import annotations
+
+from pathway.internals import asynchronous, universes
+from pathway.internals._reducers import reducers
+from pathway.internals.api import Pointer
+from pathway.internals.common import (
+    apply,
+    apply_async,
+    apply_with_type,
+    cast,
+    coalesce,
+    declare_type,
+    if_else,
+    iterate,
+    make_tuple,
+    numba_apply,
+    require,
+    udf,
+)
+from pathway.internals.decorators import (
+    attribute,
+    input_attribute,
+    input_method,
+    method,
+    output_attribute,
+    transformer,
+)
+from pathway.internals.expression import (
+    ColumnExpression,
+    ColumnExpressionOrConst,
+    ColumnIxExpression,
+    ColumnReference,
+    ReducerExpression,
+    ReducerIxExpression,
+)
+from pathway.internals.groupby import GroupedJoinResult, GroupedTable
+from pathway.internals.join import FilteredJoinResult, Joinable, JoinResult
+from pathway.internals.join_mode import JoinMode
+from pathway.internals.monitoring import MonitoringLevel
+from pathway.internals.operator import iterate_universe
+from pathway.internals.row_transformer import ClassArg
+from pathway.internals.run import run, run_all
+from pathway.internals.schema import (
+    Schema,
+    column_definition,
+    schema_builder,
+    schema_from_types,
+)
+from pathway.internals.sql import sql
+from pathway.internals.table import Table
+from pathway.internals.table_like import TableLike
+from pathway.internals.table_slice import TableSlice
+from pathway.internals.thisclass import left, right, this
+from pathway.internals.version import __version__
+
+__all__ = [
+    "JoinMode",
+    "ClassArg",
+    "declare_type",
+    "cast",
+    "reducers",
+    "apply",
+    "udf",
+    "apply_async",
+    "apply_with_type",
+    "attribute",
+    "input_attribute",
+    "input_method",
+    "iterate",
+    "method",
+    "output_attribute",
+    "transformer",
+    "iterate_universe",
+    "schema_from_types",
+    "GroupedTable",
+    "GroupedJoinResult",
+    "JoinResult",
+    "FilteredJoinResult",
+    "IntervalJoinResult",
+    "Table",
+    "TableLike",
+    "ColumnReference",
+    "ColumnExpression",
+    "ColumnExpressionOrConst",
+    "ReducerExpression",
+    "Schema",
+    "Pointer",
+    "MonitoringLevel",
+    "WindowJoinResult",
+    "this",
+    "left",
+    "right",
+    "Joinable",
+    "coalesce",
+    "require",
+    "if_else",
+    "make_tuple",
+    "sql",
+    "run",
+    "run_all",
+    "numba_apply",
+    "ColumnIxExpression",
+    "ReducerIxExpression",
+    "__version__",
+    "universes",
+    "asynchronous",
+    "AsofJoinResult",
+    "schema_builder",
+    "column_definition",
+    "TableSlice",
+]
